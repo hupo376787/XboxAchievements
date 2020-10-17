1ba64d287556c330b6ed3acb9fa4017a906b801a
Vincent Wang <376787823@qq.com>
2020-10-17 03:19:17 -0500

    Add files via upload

---
+++ XboxAchievements/AchievementsPopup.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using System;
+using Windows.Graphics.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Graphics.Display;
+using System.Collections.Generic;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace XboxAchievements
+{
+    public sealed partial class AchievementsPage : Page, INotifyPropertyChanged
+    {
+        public AchievementsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        void RaisePropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void TextBlock_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            achiBorder.Width = txtContent.ActualWidth + 200;
+        }
+
+        private void TextBoxHeader_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            txtHeader.Text = (sender as TextBox).Text.Trim();
+        }
+        private void TextBoxContent_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtContent.Text = (sender as TextBox).Text.Trim();
+        }
+
+        private async void Button_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Image", new List<string>() { ".png" });
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                RenderTargetBitmap rtb = new RenderTargetBitmap();
+                await rtb.RenderAsync(panel);
+
+                var pixelBuffer = await rtb.GetPixelsAsync();
+                var pixels = pixelBuffer.ToArray();
+                var displayInformation = DisplayInformation.GetForCurrentView();
+                using (var stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, stream);
+                    encoder.SetPixelData(BitmapPixelFormat.Bgra8,
+                                         BitmapAlphaMode.Premultiplied,
+                                         (uint)rtb.PixelWidth,
+                                         (uint)rtb.PixelHeight,
+                                         displayInformation.RawDpiX,
+                                         displayInformation.RawDpiY,
+                                         pixels);
+                    await encoder.FlushAsync();
+                }
+            }
+        }
+    }
+}
